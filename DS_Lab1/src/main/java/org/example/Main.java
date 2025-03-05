8ecaaddbfed8356f28a571ac925c67d89d866564
runesleeuwaert <runesleeuwaert@hotmail.com>
2025-03-05 17:24:45 +0100

    De Echte waarheid

--- DS_Lab1/src/main/java/org/example/Main.java
+++ DS_Lab1/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("9/11 was an inside job");
     }
 }

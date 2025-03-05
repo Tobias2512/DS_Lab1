bfcb8f7e71601d6101496a3ae6d9dff71315e789
Tobias2512 <hagenaarstobias@gmail.com>
2025-03-05 17:32:31 +0100

    De effectieve waarheid

--- DS_Lab1/src/main/java/org/example/Main.java
+++ DS_Lab1/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("9/11 was an inside job");
+        System.out.println("9/11 was not an inside job");
     }
 }
